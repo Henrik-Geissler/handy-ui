7bb14b63b2b7e8c0cc39b96a20a835e5ed59477f
Henrik Geißler <henrik.geissler@gmail.com>
2021-01-04 19:28:49 +0100

    feat(test): UNIT test for If

--- src/components/If/If.test.tsx
+++ src/components/If/If.test.tsx
@@ -6,13 +6,36 @@ import React from 'react'
 
 import If from './If'
 
-test('CheckboxWithLabel changes the text after click', () => {
-  const component = shallow(<If is={false} />)
-  // Interaction demo
-  // expect(checkbox.text()).toEqual('Off')
-  // checkbox.find('input').simulate('change')
-  // expect(checkbox.text()).toEqual('On')
-  // Snapshot demo
-  console.log(component)
-  expect(shallow).toMatchSnapshot()
+test('<If> shows nothing on is=false', () => {
+  const test = shallow(
+    <If is={false}>
+      <p className='inside'>Hello</p>
+    </If>
+  )
+  expect(test.find('.inside')).toHaveLength(0)
+  expect(test).toEqual({})
+})
+test('<If> shows 1 child on is=true', () => {
+  const test = shallow(
+    <If is>
+      <p className='inside'>Hello</p>
+    </If>
+  )
+  expect(test.find('.inside')).toHaveLength(1)
+})
+test('<If> shows multiple child on is=true', () => {
+  const test = shallow(
+    <If is>
+      <p className='inside'>Hello1</p>
+      <p className='inside'>Hello2</p>
+      <p className='inside'>Hello3</p>
+    </If>
+  )
+  expect(test.find('.inside')).toHaveLength(3)
+})
+test('<If> shows nothing if no children', () => {
+  const test = shallow(<If is={false} />)
+  expect(test).toEqual({})
+  const test2 = shallow(<If is />)
+  expect(test2).toEqual({})
 })
