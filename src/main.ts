f075fdcdb790db9d72f57a41115b8305bcfc7746
Henrik Geißler <62219051+Henrik-Geissler@users.noreply.github.com>
2021-01-06 23:32:26 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,6 @@
 export { default as If } from './components/If/If'
 export { default as Resource } from './components/Resource/ResourceContainer'
 export { default as Space } from './components/Space/SpaceContainer'
+export { default as Page } from './components/Page/PageContainer'
+export { default as Error } from './components/Error/Error'
+export { default as Toast } from './components/Toast/Toast'
