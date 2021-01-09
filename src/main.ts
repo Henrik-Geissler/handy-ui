a3f0daec979751b58a74f8eb52ddb4f47a8e06c8
CI: Lint <action@github.com>
2021-01-09 17:58:33 +0000

    ci(lint): Lint & Format

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 /**
  * Copyright (c) 2021, Henrik Geißler.
  */
-  export { default as Error } from './components/Error/Error'
-  export { default as Fallback } from './components/Error/Error'
-  export { default as If } from './components/If/If'
-  export { default as Page } from './components/Page/PageContainer'
-  export { default as Resource } from './components/Resource/ResourceContainer'
-  export { default as Space } from './components/Space/SpaceContainer'
-  export { default as Style } from './components/Style/Style'
-  export { default as Toast } from './components/Toast/Toast'
+export { default as Error } from './components/Error/Error'
+export { default as Fallback } from './components/Error/Error'
+export { default as If } from './components/If/If'
+export { default as Page } from './components/Page/PageContainer'
+export { default as Resource } from './components/Resource/ResourceContainer'
+export { default as Space } from './components/Space/SpaceContainer'
+export { default as Style } from './components/Style/Style'
+export { default as Toast } from './components/Toast/Toast'
 
-  // WIP
+// WIP
 
-  export { default as BodyWrapper } from './components/BodyWrapper/BodyWrapper'
-  export { default as ErrorBoundary } from './components/ErrorBoundary/ErrorBoundaryClass'
-  export { default as RealHeight } from './components/RealHeight/RealHeight'
-  export { default as UnlockAudio } from './components/UnlockAudio/UnlockAudio'
+export { default as BodyWrapper } from './components/BodyWrapper/BodyWrapper'
+export { default as ErrorBoundary } from './components/ErrorBoundary/ErrorBoundaryClass'
+export { default as RealHeight } from './components/RealHeight/RealHeight'
+export { default as UnlockAudio } from './components/UnlockAudio/UnlockAudio'
